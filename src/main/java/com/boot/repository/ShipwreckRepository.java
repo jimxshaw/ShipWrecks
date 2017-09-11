b399ab3715dcba7e695186ff97ce8be4cd742800
Jim Shaw <jimxshaw@gmail.com>
2017-09-10 19:12:17 -0500

    Add integration test class.

--- src/main/java/com/boot/repository/ShipwreckRepository.java
+++ src/main/java/com/boot/repository/ShipwreckRepository.java
@@ -4,5 +4,5 @@ import com.boot.model.Shipwreck;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ShipwreckRepository extends JpaRepository<Shipwreck, Long> {
-    
+
 }
